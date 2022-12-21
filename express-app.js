d4489080bd6120b5a501bd9be31115cc2cf3ab13
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-22 08:26:09 +0900

    add ejs to render dynamic data inside HTML

--- express-app.js
+++ express-app.js
@@ -6,19 +6,37 @@ const app = express();
 const hostname = "localhost";
 const port = 3000;
 
+/* register view engine */
+app.set("view engine", "ejs");
+/* specify the views directory */
+app.set("views", "./views");
+
 /* listen http request */
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+/* mock data */
+const blogs = [
+  {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+];
+
 app.get("/", (req, res) => {
   // res.status(200).send('<p>Hello world from express</p>');
   /* path must be absolute or specify root to res.sendFile */
-  res.sendFile("./views/index.html", { root: __dirname });
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  // res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 /* redirects */
@@ -29,5 +47,6 @@ app.get("/about-us", (req, res) => {
 /* 404 page */
 /* 'use' means use this function if reach this line of code. */
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  // res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: 404 });
 });
