ee2b06826d1ffcd4f765c73b39f7ed631ce0b2b7
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-22 17:40:28 +0900

    add DELETE request function

--- express-app.js
+++ express-app.js
@@ -67,19 +67,44 @@ app.get("/blogs", (req, res) => {
     });
 });
 
-app.post("/blogs", (req, res) => {
+app.post("/blogs", async (req, res) => {
   // console.log(req.body); // { title: 'blog 2', snippet: 'snippet 2', body: 'content 2' }
   const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => res.redirect("/blogs"))
-    .catch((err) => {
-      console.log(err);
-    });
+  // blog
+  //   .save()
+  //   .then((result) => res.redirect("/blogs"))
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  try {
+    await blog.save();
+    res.redirect("/blogs");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
+});
+
+app.get("/blogs/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  try {
+    res.render("details", { blog, title: "Blog Details" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.delete("/blogs/:id", async (req, res) => {
+  try {
+    const blogToDelete = await Blog.findByIdAndDelete(req.params.id);
+    // in ajax request, we have to use json to redirect
+    res.json({ redirect: "/blogs" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 /* redirects */
