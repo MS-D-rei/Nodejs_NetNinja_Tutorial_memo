0b4a9cbf867831acac5773359f2f42117d67c029
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-22 19:38:25 +0900

    add controller for blog and refactor code of blogRoute.js

---
+++ controllers/blogController.js
@@ -0,0 +1,61 @@
+const Blog = require("../models/Blog");
+
+const blog_index = async (req, res) => {
+  try {
+    const allBlog = await Blog.find().sort({ createdAt: -1 });
+    res.render("./blogs/index", { title: "All blogs", blogs: allBlog });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const blog_details = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    res.render("./blogs/details", { title: "Blog Details", blog });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const blog_create_get = (req, res) => {
+  res.render("./blogs/create", { title: "Create a new blog" });
+};
+
+const blog_create_post = async (req, res) => {
+  // console.log(req.body); // { title: 'blog 2', snippet: 'snippet 2', body: 'content 2' }
+  const blog = new Blog(req.body);
+  try {
+    await blog.save();
+    res.redirect("/blogs");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const blog_delete = async (req, res) => {
+  try {
+    const blogToDelete = await Blog.findByIdAndDelete(req.params.id);
+    console.log(blogToDelete);
+    /*{
+      _id: new ObjectId("63a41ad82374b67a5c231745"),
+      title: 'New Blog 3',
+      snippet: 'snippet 2',
+      body: 'content 3',
+      createdAt: 2022-12-22T08:52:40.516Z,
+      updatedAt: 2022-12-22T08:52:40.516Z,
+      __v: 0
+    }*/
+    res.json({redirect: "/blogs"});
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = {
+  blog_index,
+  blog_details,
+  blog_create_get,
+  blog_create_post,
+  blog_delete,
+};
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,58 +1,59 @@
 const express = require("express");
-const Blog = require("../models/Blog");
-
 const router = express.Router();
+const { blog_index, blog_details, blog_create_get, blog_create_post, blog_delete } = require('../controllers/blogController')
 
-router.get("/", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.post("/", async (req, res) => {
-  // console.log(req.body); // { title: 'blog 2', snippet: 'snippet 2', body: 'content 2' }
-  const blog = new Blog(req.body);
-  // blog
-  //   .save()
-  //   .then((result) => res.redirect("/blogs"))
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-  try {
-    await blog.save();
-    res.redirect("/blogs");
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-router.get("/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
-
-router.get("/:id", async (req, res) => {
-  const blog = await Blog.findById(req.params.id);
-  try {
-    res.render("details", { blog, title: "Blog Details" });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    const blogToDelete = await Blog.findByIdAndDelete(req.params.id);
-    // in ajax request, we have to use json to redirect
-    console.log(blogToDelete);
-    res.json({ redirect: "/blogs" });
-  } catch (err) {
-    console.log(err);
-  }
-});
+/* pass the reference of controller function to router as callback */
+router.get("/", blog_index);
+router.post("/", blog_create_post);
+router.get("/create", blog_create_get);
+router.get("/:id", blog_details);
+router.delete("/:id", blog_delete);
 
 module.exports = router;
+
+/* move logics below to controller file */
+// router.get("/", (req, res) => {
+//   Blog.find()
+//     .sort({ createdAt: -1 })
+//     .then((result) => {
+//       res.render("index", { title: "All blogs", blogs: result });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// router.post("/", async (req, res) => {
+//   // console.log(req.body); // { title: 'blog 2', snippet: 'snippet 2', body: 'content 2' }
+//   const blog = new Blog(req.body);
+//   try {
+//     await blog.save();
+//     res.redirect("/blogs");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// router.get("/create", (req, res) => {
+//   res.render("create", { title: "Create a new blog" });
+// });
+
+// router.get("/:id", async (req, res) => {
+//   const blog = await Blog.findById(req.params.id);
+//   try {
+//     res.render("details", { blog, title: "Blog Details" });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const blogToDelete = await Blog.findByIdAndDelete(req.params.id);
+//     // in ajax request, we have to use json to redirect
+//     console.log(blogToDelete);
+//     res.json({ redirect: "/blogs" });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
